2ff41370f0581c44130ade76e387f8cd2fd8df5b
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-11-23 01:03:41 +0700

    Updated.

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,28 @@ import "../css/app.scss"
 import Vue from 'vue'
 import Index from "./vue-components/Index"
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAkGu5acoJVMnYuFtUsUlVhati07rP9CGc",
+    authDomain: "pomodoro-f7e3a.firebaseapp.com",
+    databaseURL: "https://pomodoro-f7e3a.firebaseio.com",
+    projectId: "pomodoro-f7e3a",
+    storageBucket: "pomodoro-f7e3a.appspot.com",
+    messagingSenderId: "182294179496",
+    appId: "1:182294179496:web:a3c51953995400035a9c9d",
+    measurementId: "G-DLWMZWFS0B"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
     el: "#app",
--- src/js/vue-components/Index.vue
+++ src/js/vue-components/Index.vue
@@ -1,18 +1,99 @@
 <template>
-  <div>
-      <h1>
-          Pomodoro Bullshit
-      </h1>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, maxime sed atque dolor voluptatum neque nulla vitae eius laborum facilis dignissimos ea nobis reiciendis illo magnam. Voluptatum eum incidunt aperiam!
-    </p>
-  </div>
-  
+    <div class="container">
+        <div class="card mt-5">
+            <div class="card-body">
+                <h1> Awesome Pomodoro App </h1>
+
+                <template v-show="currentUser">
+                    <div class="alert alert-info">
+                        Welcome, {{ currentUser.displayName }}
+                    </div>
+                </template>
+
+                <template v-show="!currentUser">
+                    <div class="alert alert-warning">
+                        <i class="fa fa-warning"></i>
+                        You haven't signed in yet. Click the sign in / sign up button in order to sign in if you have already registered your account, to sign up if you haven't.
+                    </div>
+
+                    <button 
+                        @click="attemptSignIn"
+                        class="btn btn-dark"
+                        >
+                        Sign In using Google
+                        <i class="fa fa-google"></i>
+                    </button>
+                </template>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
 
+import * as firebase from "firebase/app";
+
+
+export default {
+    mounted() {
+        // this.googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+        
+
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                this.currentUser = user
+                this.authStatus = true
+                return
+            }
+            else {
+                this.authStatus = false
+            }
+        });
+
+        
+        // firebase.auth().signInWithPopup(provider).then(function(result) {
+        //     // This gives you a Google Access Token. You can use it to access the Google API.
+        //     var token = result.credential.accessToken;
+        //     // The signed-in user info.
+        //     var user = result.user;
+        //     // ...
+        // })
+        // .catch(function(error) {
+        //     // Handle Errors here.
+        //     var errorCode = error.code;
+        //     var errorMessage = error.message;
+        //     // The email of the user's account used.
+        //     var email = error.email;
+        //     // The firebase.auth.AuthCredential type that was used.
+        //     var credential = error.credential;
+        //     // ...
+        // });
+    },
+
+    data() {
+        return {
+            authStatus: null,
+            currentUser: null,
+        }
+    },
+
+    methods: {
+        attemptSignIn() {
+            firebase.auth()
+                .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+                .then(() => {
+                    return firebase.auth().signInWithPopup(this.googleAuthProvider)
+                })
+                .then(result => {
+                    this.currentUser = result.user
+                })
+                .catch(error => {
+                    console.log(error)
+                    alert("INVALID SIGN IN")
+                })
+        },
+    }
 }
 </script>
 
